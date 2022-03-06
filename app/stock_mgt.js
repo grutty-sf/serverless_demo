d34e3b44cfa056fc6a504316c729ab9267a7d4ca
xuan <software.grutty@gmail.com>
2022-03-06 18:53:35 +0800

    fix

--- app/stock_mgt.js
+++ app/stock_mgt.js
@@ -1,4 +1,5 @@
 exports.handler = async function(event, context) {
+    console.log('sqs handler')
     event.Records.forEach(record => {
       const { body } = record;
       console.log(`sqs body ${body}`);
