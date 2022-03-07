52b03ce3e40a0bd59f14fb27ebe9c6f2e1192fd2
xuan <573924280@qq.com>
2022-03-07 14:58:41 +0800

    fix

--- app/stock_mgt.js
+++ app/stock_mgt.js
@@ -1,20 +1,18 @@
 const AWS = require("aws-sdk");
 const DB = new AWS.DynamoDB.DocumentClient();
 
-const dynamodb = require('aws-sdk/clients/dynamodb');
-const docClient = new dynamodb.DocumentClient();
 const tableName = process.env.STOCK_HOLD_TABLE;
 
 exports.handler = async (event, context) => {
     console.log('sqs handler')
     event.Records.forEach(record => {
       let body = JSON.parse(record.body)
-      console.log(`sqs body ${body}`);
+      console.log(`sqs body ${JSON.stringify(body)}`);
 
       let actionHold = [];
       let actionOther = [];
 
-      body.forEach(item => {
+      for (const item of body) {
         if(item.action == 'hold') {
           actionHold.push({
             PutRequest: { Item: item },
@@ -24,10 +22,9 @@ exports.handler = async (event, context) => {
             PutRequest: { Item: item.obj },
           })
         }
-      })
-      console.log('stock data', JSON.stringify(actionHold), JSON.stringify(actionOther));
+      }
+
       try {
-        console.log(db, JSON.stringify(DB));
         if(actionOther.length > 0) {
           DB.batchWrite({
             RequestItems: {
@@ -39,24 +36,9 @@ exports.handler = async (event, context) => {
         if(actionHold.length > 0) {
           DB.batchWrite({
             RequestItems: {
-              [tableName]: actionHold
+              ECOM_stock_hold: actionHold
             },
           }).promise()
-
-          /* docClient.put(
-            {
-              action: "hold",
-              ref: `checkout_id${id}`,
-              obj: {
-                  stock_id: `stock_id${id}`,
-                  created_at: new Date().toISOString(),
-                  action: "hold",
-                  action_amount: 123,
-                  reference: `checkout_id${id}`,
-                  stock_amount: 10,
-              }
-            }
-          ).promise(); */
         }
       } catch (error) {
         console.log('123', error);
